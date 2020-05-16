12976b03d3d84bbab8bf93bc4e25d9711e359ab4
Karim Abdelgwad <karim.elbadry2@gmail.com>
2020-05-16 13:54:15 +0200

    -Nothing

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -2,85 +2,85 @@
 
 @section('content')
     <div class="form-container" >
-    <form method="post" action="{{route("admin.terms.action")}}">
-        @method('PUT')
-        @csrf
-        @if (session('status'))
-            <div class="alert-success">
-                {{ session('status') }}
+        <form method="post" action="{{route("admin.terms.action")}}">
+            @method('PUT')
+            @csrf
+            @if (session('status'))
+                <div class="alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <div class="splitter">
+                <h4 class="unique">المتصفح:</h4>
+                <span></span>
             </div>
-        @endif
-        <div class="splitter">
-            <h4 class="unique">المتصفح:</h4>
-            <span></span>
-        </div>
-        <div class="section">
-            <div class="input-container">
-                <label for="icon">ايقونة الموقع:</label>
-                <input placeholder="اكتب عنوان الرسالة..." type="file" name="icon" value="" autofocus>
+            <div class="section">
+                <div class="input-container">
+                    <label for="icon">ايقونة الموقع:</label>
+                    <input placeholder="اكتب عنوان الرسالة..." type="file" name="icon" value="" autofocus>
+                </div>
+                <div class="input-container">
+                    <label for="title">اسم الموقع:</label>
+                    <input placeholder="اكتب عنوان الموقع..." type="text" name="title" value="" autofocus>
+                </div>
+                <div class="input-container">
+                    <label for="subtitle">عنوان فرعي:</label>
+                    <input placeholder="اكتب عنوان فرعي..." type="text" name="subtitle" value="" autofocus>
+                </div>
+                <div class="input-container flex-2">
+                    <label for="description">وصف بسيط:</label>
+                    <input placeholder="اكتب وصف الموقع..." type="text" name="description" value="" autofocus>
+                </div>
             </div>
-            <div class="input-container">
-                <label for="title">اسم الموقع:</label>
-                <input placeholder="اكتب عنوان الموقع..." type="text" name="title" value="" autofocus>
+            <div class="splitter">
+                <h4 class="unique">الموقع:</h4>
+                <span></span>
             </div>
-            <div class="input-container">
-                <label for="subtitle">عنوان فرعي:</label>
-                <input placeholder="اكتب عنوان فرعي..." type="text" name="subtitle" value="" autofocus>
+            <div class="section">
+                <div class="input-container">
+                    <label for="logo">شعار الموقع:</label>
+                    <input placeholder="اكتب عنوان الرسالة..." type="file" name="logo" value="" autofocus>
+                </div>
+                <div class="flex-4"></div>
             </div>
-            <div class="input-container flex-2">
-                <label for="description">وصف بسيط:</label>
-                <input placeholder="اكتب وصف الموقع..." type="text" name="description" value="" autofocus>
+            <div class="splitter">
+                <h4 class="unique">ادوات جوجل</h4>
+                <span></span>
             </div>
-        </div>
-        <div class="splitter">
-            <h4 class="unique">الموقع:</h4>
-            <span></span>
-        </div>
-        <div class="section">
-            <div class="input-container">
-                <label for="logo">شعار الموقع:</label>
-                <input placeholder="اكتب عنوان الرسالة..." type="file" name="logo" value="" autofocus>
+            <div class="section">
+                <div class="input-container">
+                    <label for="google_id">كود مصادقة جوجل:</label>
+                    <input placeholder="اكتب كود مصادقة جوجل..." type="text" name="google_id" value="" autofocus>
+                </div>
+                <div class="input-container">
+                    <label for="google">كود مصادقة جوجل:</label>
+                    <input placeholder="اكتب كود مصادقة جوجل..." type="text" name="google" value="" autofocus>
+                </div>
+                <div class="flex-3"></div>
             </div>
-            <div class="flex-4"></div>
-        </div>
-        <div class="splitter">
-            <h4 class="unique">ادوات جوجل</h4>
-            <span></span>
-        </div>
-        <div class="section">
-            <div class="input-container">
-                <label for="google_id">كود مصادقة جوجل:</label>
-                <input placeholder="اكتب كود مصادقة جوجل..." type="text" name="google_id" value="" autofocus>
+            <div class="splitter">
+                <h4 class="unique">الشبكات الاجتماعية:</h4>
+                <span></span>
             </div>
-            <div class="input-container">
-                <label for="google">كود مصادقة جوجل:</label>
-                <input placeholder="اكتب كود مصادقة جوجل..." type="text" name="google" value="" autofocus>
+            <div class="section">
+                <div class="input-container">
+                    <label for="facebook">فيسبوك:</label>
+                    <input placeholder="فيسبوك..." type="text" name="facebook" value="" autofocus>
+                </div>
+                <div class="input-container">
+                    <label for="twitter">تويتر:</label>
+                    <input placeholder="تويتر..." type="text" name="twitter" value="" autofocus>
+                </div>
+                <div class="input-container">
+                    <label for="instagram">انستجرام:</label>
+                    <input placeholder="انستجرام..." type="text" name="instagram" value="" autofocus>
+                </div>
+                <div class="flex-2"></div>
             </div>
-            <div class="flex-3"></div>
-        </div>
-        <div class="splitter">
-            <h4 class="unique">الشبكات الاجتماعية:</h4>
-            <span></span>
-        </div>
-        <div class="section">
-            <div class="input-container">
-                <label for="facebook">فيسبوك:</label>
-                <input placeholder="فيسبوك..." type="text" name="facebook" value="" autofocus>
-            </div>
-            <div class="input-container">
-                <label for="twitter">تويتر:</label>
-                <input placeholder="تويتر..." type="text" name="twitter" value="" autofocus>
-            </div>
-            <div class="input-container">
-                <label for="instagram">انستجرام:</label>
-                <input placeholder="انستجرام..." type="text" name="instagram" value="" autofocus>
-            </div>
-            <div class="flex-2"></div>
-        </div>
-        <button type="submit">حفظ</button>
-    </form>
+            <button type="submit">حفظ</button>
+        </form>
 
-    <div class="splitter">
+        <div class="splitter">
             <h4 class="unique">المواقع التابع:</h4>
             <span></span>
         </div>
