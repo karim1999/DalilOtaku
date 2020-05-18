7c17d4dd86cec5d4e56c43a240bb222fa0772410
Karim Abdelgwad <karim.elbadry2@gmail.com>
2020-05-18 22:28:46 +0200

    Anime Api Fetch

--- resources/js/anime.js
+++ resources/js/anime.js
@@ -11,23 +11,36 @@ const anime = new Vue({
     data: {
         isLoadingAnime: false,
         mainEndPoint: 'https://api.jikan.moe/v3/',
-
+        axiosConfig: {
+            baseURL: 'https://api.jikan.moe/v3/',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+                'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+        }
     },
     methods: {
         loadAnime(){
             this.isLoadingAnime= true
-            return axios.get(this.mainEndPoint + 'season/archive').then(res => {
-                console.log(res.data.archive)
-                res.data.archive.forEach(year => {
-                    year.seasons.forEach(season => {
-                        console.log("year:", year.year, "Season:", season)
-                        // return "hi"
-                        // axios.get(this.mainEndPoint + year.year + '/' + season).then(res => {
-                        //     console.log(res.data)
-                        // })
-                    })
-                })
+            return axios.get(this.mainEndPoint+ 'season/archive').then(async res => {
+                console.log(res.data.archive);
+                for (let i= 0; i < res.data.archive.length; i++){
+                    let year= res.data.archive[i]
+                    for (let j= 0; j < year.seasons.length; j++){
+                        let season= year.seasons[j]
+                        await axios.get(this.mainEndPoint+ 'season/' + year.year + '/' + season.toLowerCase(), {
+                            headers: {
+                            }
+                        }).then(res => {
+                            console.log(res.data)
+                        })
+                    }
+
+                }
             }).then(() => {
+                console.log("done")
                 this.isLoadingAnime= false
             }).catch(error => {
                 console.log(error)
