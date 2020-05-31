3c70cf5742fd8f335a0a6a4dd88b60526085f7a8
Karim Abdelgwad <karim.elbadry2@gmail.com>
2020-05-31 17:11:05 +0200

    Cron job Daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
         $schedule->call(function () {
             $anime= new AnimeController;
             $anime->fetch_data_from_server();
-        })->everyFiveMinutes();
+        })->daily();
     }
 
     /**
